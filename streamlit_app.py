6ba70c0e228f8ace6a86a00b2e966527c11028fb
joseesparzarsnd <78566958+joseesparzarsnd@users.noreply.github.com>
2024-07-19 12:07:00 -0600

    Update streamlit_app.py
    Corrected error in column name

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.write("The name on your smoothie will be: ", name_on_smoothie)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('NAME_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 st.dataframe(data = my_dataframe, use_container_width = True)
 st.stop()
 
